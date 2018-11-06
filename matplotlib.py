6ae4b62ae81864b155f52639e51c7e33dc810173
yuliiakandych <y_kandych@fanshaweonline.ca>
2018-11-06 14:22:15 -0500

    basic

---
+++ matplotlib.py
@@ -0,0 +1,9 @@
+# basic matplotlib stuff
+#
+import matplotlib.pyplot as plt
+
+years = [1900, 1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1995, 2000, 2005, 2010, 2015]
+pops = [1.6, 2.5, 2.6, 3.0, 3.3, 3.6, 4.2, 4.4, 4.8, 5.3, 5.7, 6.1, 6.5, 6.9, 7.3]
+
+plt.plot(year, pops)
+plt.show()
